8e11861c1373d1418c6cc5fdc3bd6078401f6bfc
Dmitry Stepanenko <developer.stepanenko@gmail.com>
2018-03-22 19:29:36 +0300

    specify python3

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
-pip install -r requirements
-python manage.py migrate
-python manage.py collectstatic --noinput
+pip install -r requirements.txt
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput

d0172f2480c6a8ad71b4ed860d8d010b0f544df8
Dmitry Stepanenko <developer.stepanenko@gmail.com>
2018-03-23 13:21:32 +0300

    add postgresql config example

--- local_settings.example.py
+++ local_settings.example.py
@@ -1,2 +1,16 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'CHANGEME'
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CHANGEME',
+        'USER': 'CHANGEME',
+        'PASSWORD': 'CHANGEME',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
